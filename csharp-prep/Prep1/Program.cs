604826967082a4893ec06c584820be71c976bc7c
NdikumNdifor <u0113322020@byui.edu>
2023-09-09 19:47:47 +0100

    Practicing C# Input and Output Statements

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        // The "Console.WriteLine()" is used to 
+        // creat white space for cleaner readability
+        Console.WriteLine();
+
+        // Prompts the user for their first name
+        Console.Write("What is your first name? ");
+        string firstName = Console.ReadLine();
+        
+        // prompts the user for their Lastname
+        Console.Write("What is your last name? ");
+        string lastName = Console.ReadLine();
+
+        Console.WriteLine();
+
+        // Prints out a statement to the screen
+        Console.WriteLine($"My name is {lastName}, {firstName} {lastName}.");
+        Console.WriteLine();
+
     }
 }
